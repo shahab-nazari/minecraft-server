adafdd19964f6703bc0d8f93ab407cd7a6d3dab9
Shahab Nazari <shahab.website@gmail.com>
2025-11-26 14:47:36 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# گوگل: دانلود PaperMC
-curl -o paper.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/100/downloads/paper-1.20.1-100.jar
 
-# اجرای سرور
+# دانلود
+curl -o paper-1.20.1-100.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/100/downloads/paper-1.20.1-100.jar
 
-java -Xmx2G -Xms2G -jar paper-1.20.1-100.jar nogui
+# تغییر نام فایل
+mv paper-1.20.1-100.jar paper.jar
+
+# ساخت eula
+echo "eula=true" > eula.txt
+
+# اجرا
+java -Xmx2G -Xms2G -jar paper.jar nogui
